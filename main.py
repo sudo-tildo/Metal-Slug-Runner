9180abe3702906a753be365b8fda5ce58c62b538
Breno de Moraes (Tildo) <breno.moraes1809@gmail.com>
2023-05-23 23:47:30 -0300

    Música adicionada

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from dino_runner.components.game import Game
+import pygame
 
+
+pygame.init()
+pygame.mixer.music.load('METAL-SLUG-1-STAGE-1.mp3')
+pygame.mixer.music.play(-1)
 
 if __name__ == "__main__":
     game = Game()
@@ -7,4 +12,4 @@ if __name__ == "__main__":
     game.execute()
 
 
-
+    
